3d8755cf4f6ebb60b9c9c3a943876e2cd9d8c47b
Arcane <lbenzene@qq.com>
2015-09-10 13:38:14 +0800

    add #xxxxxx transfrom method

--- chrome_tool/tool.js
+++ chrome_tool/tool.js
@@ -5,6 +5,25 @@ function $(id) {
     return document.getElementById(id);
 }
 
+$.prototype.css = function(attr, value) {
+    switch (arguments.length) {
+        case 1:
+            if (typeof arguments[0] == "object") {    //批量设置属性
+                for (var i in attr) this.style[i] = attr[i]
+            }
+            else {    // 读取属性值
+                return this.currentStyle ? this.currentStyle[attr] : getComputedStyle(this, null)[attr]
+            }
+            break;
+        case 2:
+            //设置属性
+            this.style[attr] = value;
+            break;
+        default:
+            return "";
+    }
+};
+
 function css(obj, attr, value) {
     switch (arguments.length) {
         case 2:
@@ -29,7 +48,8 @@ function hasClass(obj, cls) {
 }
 
 function addClass(obj, cls) {
-    if (!this.hasClass(obj, cls)) obj.className += " " + cls;
+    if (!this.hasClass(obj, cls))
+        obj.className += " " + cls;
 }
 
 function removeClass(obj, cls) {
@@ -39,10 +59,51 @@ function removeClass(obj, cls) {
     }
 }
 
+String.prototype.trim = function() {
+   return this.replace(/(^\s*)|(\s*$)/g, "");
+}
+String.prototype.ltrim = function() {
+   return this.replace(/(^\s*)/g, "");
+}
+String.prototype.rtrim = function() {
+   return this.replace(/(\s*$)/g, "");
+}
+
+function switch_color(cstr) {
+    cstr = cstr.trim();
+    var R, G, B;
+    var match;
+    if (match = cstr.match(new RegExp('^#([0-9a-fA-F]{2}){3}'))) {
+        R = parseInt(match[0].substr(1, 2), 16);
+        G = parseInt(match[0].substr(3, 2), 16);
+        B = parseInt(match[0].substr(5, 2), 16);
+        // console.log(R);
+        // console.log(G);
+        // console.log(B);
+    } else if (true) {
+
+    };
+    if (R + B + G < 127 * 3 ) {
+        $('#colour-transform-output').style.color = "#EBEBEB";
+    } else {
+        $('#colour-transform-output').style.color = "#141414";
+    }
+    $('#colour-transform-output').style.backgroundColor = "rgb(" + R + "," + G + "," + B + ")";
+    // $('#colour-transform-output').css('backgroundColor', "rgb(" + R + "," + G + "," + B + ")");
+    var HTML = '';
+    HTML += '<a tabindex="0">#' + R.toString(16) + G.toString(16) + B.toString(16) + '</a>';
+    HTML += '<a tabindex="1">rgb(' + R + ', ' + G + ', ' + B + ')</a>'
+    $('#colour-transform-output').innerHTML = HTML;
+}
+
 function init() {
     $('#now-dispaly').addEventListener('click', function() {
-        $('#tool-list').style['display'] = 'block';
+        $('#tool-list').style['display'] = "block";
     });
+
+    $('#colour-transform-start').addEventListener('click', function() {
+        switch_color($('#colour-transform-input').value);
+    })
 }
 
 
